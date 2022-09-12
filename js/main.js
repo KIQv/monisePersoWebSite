8fa68962a863ad83e853f6d05ed3b3a05f58369c
KIQ <kaiqueoliveira257@gmail.com>
2022-09-11 22:15:33 -0700

    Inserindo arquivos no GIT

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var ul = document.querySelector('nav ul');
+var menuBtn = document.querySelector('.menu-btn i');
+
+function menuOpen() {
+    if (ul.classList.contains('open')) {
+        ul.classList.remove('open');
+    }else{
+        ul.classList.add('open');
+    }
+}
+var swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
