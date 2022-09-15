cd5b8872b63e7d89441d1aa2088fc746bef0d98c
Kaique Oliveira <kaiqueoliveira257@gmail.com>
2022-09-15 21:54:52 +0000

    Organização do codigo
    Alterações no responsivo

--- js/main.js
+++ js/main.js
@@ -1,89 +1,88 @@
-var ul = document.querySelector('nav ul');
-var menuBtn = document.querySelector('.menu-btn i');
+var ul = document.querySelector("nav ul");
+var menuBtn = document.querySelector(".menu-btn i");
 
 function menuOpen() {
-    if (ul.classList.contains('open')) {
-        ul.classList.remove('open');
-    }else{
-        ul.classList.add('open');
-    }
+  if (ul.classList.contains("open")) {
+    ul.classList.remove("open");
+  } else {
+    ul.classList.add("open");
+  }
 }
 var swiper = new Swiper(".mySwiper", {
-    effect: "fade",
-    loop: true,
-    autoplay: {
-        delay: 3500,
-        disableOnInteraction: false,
-      },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+  effect: "fade",
+  loop: true,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  mousewheel: false,
+  keyboard: true,
+});
+var swiper = new Swiper(".conteudoDestaque", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    "@0.00": {
+      slidesPerView: 1,
+      spaceBetween: 100,
     },
-    mousewheel: false,
-    keyboard: true,
-  });
-  var swiper = new Swiper(".conteudoDestaque", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+    "@0.75": {
+      slidesPerView: 2,
+      spaceBetween: 20,
     },
-    breakpoints: {
-        "@0.00": {
-          slidesPerView: 1,
-          spaceBetween: 100,
-        },
-        "@0.75": {
-          slidesPerView: 2,
-          spaceBetween: 20,
-        },
-        "@1.00": {
-          slidesPerView: 3,
-          spaceBetween: 40,
-        },
-        "@1.50": {
-          slidesPerView: 3,
-          spaceBetween: 50,
-        },
-      },
-  });
-  var swiper = new Swiper(".conteudoDepoimento", {
-    spaceBetween: 30,
-    loop: true,
-    autoplay: {
-        delay: 3500,
-        disableOnInteraction: false,
-      },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+    "@1.00": {
+      slidesPerView: 3,
+      spaceBetween: 40,
     },
-  });
-  var swiper = new Swiper(".conteudoKits", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+    "@1.50": {
+      slidesPerView: 3,
+      spaceBetween: 50,
     },
-    breakpoints: {
-        "@0.00": {
-          slidesPerView: 1,
-          spaceBetween: 10,
-        },
-        "@0.75": {
-          slidesPerView: 2,
-          spaceBetween: 20,
-        },
-        "@1.00": {
-          slidesPerView: 3,
-          spaceBetween: 40,
-        },
-        "@1.50": {
-          slidesPerView: 4,
-          spaceBetween: 50,
-        },
-      },
-  });
-  
+  },
+});
+var swiper = new Swiper(".conteudoDepoimento", {
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+var swiper = new Swiper(".conteudoKits", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    "@0.00": {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    "@0.75": {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    "@1.00": {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    "@1.50": {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
+  },
+});
