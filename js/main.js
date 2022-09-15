2757ef1fefed5ca833bafb235c642daf424d379a
Kaique Oliveira <kaiqueoliveira257@gmail.com>
2022-09-15 17:43:28 +0000

    Menu responsivo
    Alterações nos destaques, atualizando para SwiperJS

--- js/main.js
+++ js/main.js
@@ -17,3 +17,11 @@ var swiper = new Swiper(".mySwiper", {
     mousewheel: true,
     keyboard: true,
   });
+  var swiper = new Swiper(".conteudoDestaque", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
